b31a8e921296147fcc7d59872ca3ce709a3ff2a3
Assaf Weinberg <assafweinberg@gmail.com>
2010-08-28 01:27:20 -0400

    updated html/css
    Signed-off-by: Assaf Weinberg <assafweinberg@gmail.com>

---
+++ static/js/knockout.js
@@ -0,0 +1,21 @@
+var knockout = {
+	setPageMode: function(game) {
+	if(game) {
+		$("#this-game").slideDown()
+		$("#leave-game").fadeIn();
+	} else {
+		$("#this-game").slideUp()
+		$("#leave-game").fadeOut();
+	}
+	
+	},
+	
+	init: function() {
+		//login/logout
+		
+		//twitter updates
+		
+		//
+	}
+}
+

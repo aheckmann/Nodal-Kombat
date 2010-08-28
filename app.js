68f58eb8f7fa7bd99e2246035a8533a51b556d3a
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-08-27 22:00:21 -0400

    sockets are here

--- app.js
+++ app.js
@@ -7,26 +7,43 @@ require.paths.unshift(
 
 var express = require('./support/express')
   , http = require('http')
+  , io = require("./support/socketio")
 
 var app = express.createServer(
   express.errorHandler({ dumpExceptions: true, showStack: true})
 , express.logger()
 , express.cookieDecoder()
 , express.bodyDecoder()
-);
+, express.staticProvider(__dirname + '/public')
+)
+
+
+var sock = io.listen(app)
+sock.on("connection", function(client){
+  client.on("message", function(msg){
+    console.log("recieved websocket msg: " + msg)
+    client.send("what up?!")
+  })
+  client.on("disconnect", function(){
+    console.log("client disconnected")
+  })
+})
 
 app.configure(function(){
   app.set('views', __dirname + '/views')
 })
 
+
+
+// routes
 app.get('/', function(req, res){
-  res.render('index.jade', { locals: { name: "knockout" } } );    
+  res.render('index.jade', { locals: { name: "knockout" } } )
 })
 
 
-app.use(express.staticProvider(__dirname + '/public'))
-console.log("running on http://localhost:3000/")
-app.listen(3000);
+// run it!
+console.log("running on http://127.0.0.1:3000/")
+app.listen(3000)
 
 process.on("uncaughtException", function(err){
   console.warn("Caught unhandled exception:")

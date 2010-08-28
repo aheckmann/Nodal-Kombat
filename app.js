005ca89d0561f721d2a85b185cab4425ab9bbc9a
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-08-27 20:55:40 -0400

    start of basic struct

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
  
+require.paths.unshift(__dirname + '/support/express/support/connect/lib/',
+                      __dirname + '/support/jade/lib/',
+                      __dirname + '/support/express/support/connect/support/sass/lib/',
+                      __dirname + '/support/node-formidable/lib/formidable/',
+                      __dirname + '/support/connect-basic-auth/lib',
+                      __dirname + '/support/ext/lib'); //temporary
+
+
+
+var express = require('./support/express')
+  , http = require('http')
+
+var app = express.createServer(
+  express.cookieDecoder()
+, express.bodyDecoder()
+, express.staticProvider(__dirname + '/public')
+);
+
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+  res.render('index.jade', { locals: { name: "knockout" } } );    
+})
+
+
+app.listen(3000);

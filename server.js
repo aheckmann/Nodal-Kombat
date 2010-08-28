cdeeff47e8fc1ac88a1e58a707732fabb7bc80b0
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-08-28 09:01:30 -0400

    refactor oauth

--- server.js
+++ server.js
@@ -11,18 +11,12 @@ require.paths.unshift(
 )
 
 var express = require('./support/express')
-  , http = require('http')
-  , sys = require('sys')
   , io = require("./support/socketio")
   , handle = require("./lib/handle")
-	, connect = require('connect')
-	, auth= require('auth')
-	, OAuth= require('oauth').OAuth;
-
-require("./lib/redis")
 
 
-var app = express.createServer(
+
+var app = module.exports = express.createServer(
   express.errorHandler({ dumpExceptions: true, showStack: true})
 , express.logger()
 , express.cookieDecoder()
@@ -30,39 +24,12 @@ var app = express.createServer(
 , express.staticProvider(__dirname + '/public')
 )
 
-
-// setting up Twitter + OAuth
-try {
-  var example_keys= require('./keys_file');
-  for(var key in example_keys) {
-    global[key]= example_keys[key];
-  }
-  console.log('Auth key data read.');
-	app.use(connect.session());	
-	app.use(auth( [
-	  auth.Twitter({consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret})
-	]) );
-  console.log('Twitter auth enabled');
-	var oa= new OAuth("http://twitter.com/oauth/request_token",
-	                  "http://twitter.com/oauth/access_token",
-	                  twitterConsumerKey,
-	                  twitterConsumerSecret,
-	                  "1.0",
-	                  null,
-	                  "HMAC-SHA1");
-}
-catch(e) {
-  console.log('Unable to locate the keys_file.js file.  Please copy and ammend the example_keys_file.js as appropriate');
-  console.log(sys.inspect(e));
-  return;
-}
-
-
 app.configure(function(){
   app.set('views', __dirname + '/views')
 })
 
-
+require("./lib/redis")
+require("./lib/oauth")
 require("./lib/routes")(app)
 
 // sockets 
@@ -80,8 +47,8 @@ sock.on("connection", function(client){
 console.log("running on http://127.0.0.1:3000/")
 app.listen(3000)
 
+// never crash
 process.on("uncaughtException", function(err){
   console.warn("Caught unhandled exception:")
   console.warn(err.stack || err)    
 })
-

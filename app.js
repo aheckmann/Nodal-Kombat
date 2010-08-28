caca9b2f0add0136d48a32633962a0c1f1854701
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-08-27 20:57:48 -0400

    add logging

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
  
-require.paths.unshift(__dirname + '/support/express/support/connect/lib/',
-                      __dirname + '/support/jade/lib/',
-                      __dirname + '/support/express/support/connect/support/sass/lib/',
-                      __dirname + '/support/node-formidable/lib/formidable/',
-                      __dirname + '/support/connect-basic-auth/lib',
-                      __dirname + '/support/ext/lib'); //temporary
-
-
+require.paths.unshift(
+  __dirname + '/support/express/support/connect/lib/'
+, __dirname + '/support/jade/lib/'
+, __dirname + '/support/node-formidable/lib/formidable/'
+)
 
 var express = require('./support/express')
   , http = require('http')
 
 var app = express.createServer(
-  express.cookieDecoder()
+  express.logger()
+, express.cookieDecoder()
 , express.bodyDecoder()
 , express.staticProvider(__dirname + '/public')
 );

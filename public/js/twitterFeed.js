794c51508bf64332575f25e2f38f8f4e96486337
Assaf Weinberg <assafweinberg@gmail.com>
2010-08-29 11:45:35 -0400

    added twitter feed
    Signed-off-by: Assaf Weinberg <assafweinberg@gmail.com>

---
+++ public/js/twitterFeed.js
@@ -0,0 +1,28 @@
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 6,
+  interval: 6000,
+  width: 750,
+  height: 300,
+  theme: {
+    shell: {
+      background: 'transparent',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: 'transparent',
+      color: '#ffffff',
+      links: '#4aed05'
+    }
+  },
+  features: {
+    scrollbar: true,
+    loop: false,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: false,
+    behavior: 'all'
+  }
+}).render().setUser('assafweinberg').start();
